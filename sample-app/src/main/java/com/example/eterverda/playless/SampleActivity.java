67c9df16865eb832093239777131d250be5e5839
Alexander Asanov <aleksandro.eterverda@gmail.com>
2015-03-03 17:39:55 +0300

    Removed AutoUpdateService and AutoUpdateLog

--- sample-app/src/main/java/com/example/eterverda/playless/SampleActivity.java
+++ sample-app/src/main/java/com/example/eterverda/playless/SampleActivity.java
@@ -1,16 +1,12 @@
 package com.example.eterverda.playless;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import io.github.eterverda.playless.AutoUpdateService;
 
 public class SampleActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_sample);
-
-        startService(new Intent(Intent.ACTION_MAIN).setClass(this, AutoUpdateService.class));
     }
 }
