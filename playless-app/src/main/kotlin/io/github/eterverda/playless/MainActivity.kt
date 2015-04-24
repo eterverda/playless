da938c597f830fd16c0d1248daf819c95623b325
Alexander Asanov <aleksandro.eterverda@gmail.com>
2015-04-24 16:32:53 +0300

    Playless app switched to AppCompat theme

--- playless-app/src/main/kotlin/io/github/eterverda/playless/MainActivity.kt
+++ playless-app/src/main/kotlin/io/github/eterverda/playless/MainActivity.kt
@@ -1,15 +1,26 @@
 package io.github.eterverda.playless
 
 import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
 
-public class MainActivity : Activity() {
+public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ac_main)
+
+        // TODO remove when AppCompatActivity will support statusBarColor
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = getWindow()
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.setStatusBarColor(getResources().getColor(R.attr.colorPrimaryDark))
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
